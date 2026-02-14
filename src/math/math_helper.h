e23ab66e28b14aaf5ca0d2c585c4be9e4e8b05a4
rizkirakasiwi <rizkirr.xyz@gmail.com>
2026-02-15 00:48:28 +0700

    fix omp

--- src/math/math_helper.h
+++ src/math/math_helper.h
@@ -9,12 +9,16 @@
 #define NUMC_STR(x) NUMC_STR_(x)
 #define NUMC_PRAGMA(x) _Pragma(NUMC_STR(x))
 
-#define NUMC_OMP_BYTE_THRESHOLD (1 << 20) // 1 MB
+#define NUMC_OMP_BYTE_THRESHOLD (1 << 20)       // 1 MB total to enable OMP
+#define NUMC_OMP_BYTES_PER_THREAD (1 << 20)     // 1 MB minimum work per thread
 
 #define NUMC_OMP_FOR(n, elem_size, loop)                                       \
   do {                                                                         \
-    if ((n) * (elem_size) > NUMC_OMP_BYTE_THRESHOLD) {                         \
-      NUMC_PRAGMA(omp parallel for schedule(static))                           \
+    size_t _total_bytes = (n) * (elem_size);                                   \
+    if (_total_bytes > NUMC_OMP_BYTE_THRESHOLD) {                              \
+      int _nthreads = (int)(_total_bytes / NUMC_OMP_BYTES_PER_THREAD);         \
+      if (_nthreads < 1) _nthreads = 1;                                        \
+      NUMC_PRAGMA(omp parallel for schedule(static) num_threads(_nthreads))    \
       loop                                                                     \
     } else {                                                                   \
       loop                                                                     \
